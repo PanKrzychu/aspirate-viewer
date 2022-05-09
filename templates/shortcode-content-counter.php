1d7f3294895da91f8b11034c9aa03d4fa31ff499
PanKrzychu <kczachor98@gmail.com>
2022-05-09 16:10:13 +0200

    basic mockup

--- templates/shortcode-content-counter.php
+++ templates/shortcode-content-counter.php
@@ -2,16 +2,32 @@
 
 require_once plugin_dir_path(__FILE__) . '../AVApi.php';
 
+wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Catamaran&family=Source+Sans+Pro:wght@700;900&display=swap', false );
 wp_enqueue_style('av-styles', plugins_url('aspirate-viewer/templates/styles/content-counter-styles.css'));
+
+$value1 = 765;
+
 
 ?>
 
 <div class="av-content-counter-container">
-    <h2 class="av-content-counter-title">Jakiś tytuł</h2>
-    <div>
-        <div class="av-single-counter">x</div>
-        <div class="av-single-counter">y</div>
-        <div class="av-single-counter">z</div>
-        <div class="av-single-counter">za</div>
+    <h2 class="av-content-counter-title">ASPIRATE w liczbach</h2>
+    <div class="av-counter-container">
+        <div class="av-single-counter">
+            <div class="av-counter-value"><?php echo $value1 ?></div>
+            <div class="av-counter-title">Dodanych newsów marketingowych</div>
+        </div>
+        <div class="av-single-counter">
+            <div class="av-counter-value">7</div>
+            <div class="av-counter-title">Polskich liderów marketingu</div>
+        </div>
+        <div class="av-single-counter">
+            <div class="av-counter-value">7</div>
+            <div class="av-counter-title">Książek, podcastów, kursów</div>
+        </div>
+        <div class="av-single-counter">
+            <div class="av-counter-value">7</div>
+            <div class="av-counter-title">Dni istnienia</div>
+        </div>
     </div>
 </div>
